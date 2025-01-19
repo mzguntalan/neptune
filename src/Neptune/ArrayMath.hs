71e31d46a1e1c4e3a36bbf6a6973d09ca70c1a37
mzguntalan <mzguntalan@gmail.com>
2025-01-19 12:39:41 +0900

    started over; neptune as design oriented

--- src/Neptune/ArrayMath.hs
+++ src/Neptune/ArrayMath.hs
@@ -1,62 +1,73 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -Wall #-}
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-
-{-# HLINT ignore "Redundant bracket" #-}
 
 module Neptune.ArrayMath where
 
-import Jaxpr (Shape, VarType)
-
--- all arrays are currently flat
-data NeptuneArray a = NeptuneArray [a] Shape VarType
+import GHC.TypeLits
 
 -- idk
-data NaturalArray a = Array [a] | Collection [NaturalArray a]
+-- what if Neptune was a design oriented language ?
+-- you would define behaviors and shapes until you figure out the implementation details
+-- what kind of traits are important?
+-- Array type shape
+-- Positive, Negative, NonNegative, NonPositive
+-- SumTo1
+-- Between a b
 
-type Axis = Int
+data ArrayType = Af32 | Ai32 | Af16 deriving (Show)
 
-shapeSingleConcat :: Shape -> Shape -> Axis -> Shape
-shapeSingleConcat (a : as) (b : bs) axis = f (a : as) (b : bs) axis 0
+type Axis = String
+
+type Shape = [Axis]
+
+data Array = Array ArrayType Shape deriving (Show)
+
+af32 :: Shape -> Array
+af32 = Array Af32
+
+ai32 :: Shape -> Array
+ai32 = Array Ai32
+
+rank :: Array -> Int
+rank (Array _ shape) = length shape
+
+data Between (low :: Float) (high :: Float) a where
+    Between :: Float -> Float -> a -> Between low high a
+    deriving (Show)
+
+clip :: Float -> Float -> Array -> Between a b Array
+clip = Between
+
+clipNum :: Float -> Float -> Float -> Between a b Float
+clipNum a b n
+    | n > b = Between a b b
+    | n < a = Between a b a
+    | otherwise = Between a b n
+
+f :: Between a b Float -> String
+f (Between a b _)
+    | a >= 0.0 && b <= 1.0 = "Valid Range"
+    | otherwise = "Not in valid Range"
+
+mul :: Array -> Array -> Array
+mul (Array Af32 [a, b]) (Array Af32 [c, d])
+    | b == c = af32 [a, d]
+    | otherwise = error "Shape incompatible"
+mul (Array Af32 shape1) (Array Af32 shape2)
+    | length shape1 /= length shape2 = error "Shape incompatible"
+    | length shape1 > 2 = Array Af32 newshape
   where
-    f :: Shape -> Shape -> Axis -> Axis -> Shape
-    f (c : cs) (d : ds) target cur
-        | target == cur =
-            if cs == ds
-                then c + d : cs
-                else error "shape mismatch"
-        | target > cur =
-            if c == d
-                then c : f cs ds target (cur + 1)
-                else error "shape mismatch"
-        | otherwise = error "Not enough axes"
-    f _ _ _ _ = error "Should not happen"
-shapeSingleConcat _ _ _ = error "Shape should be non empty list"
-
-shapeConcat :: [Shape] -> Axis -> Shape
-shapeConcat (shape : shapes) axis = foldl (\x y -> shapeSingleStack x y axis) shape shapes
-shapeConcat [] _ = error "no shapes to concat"
-
-shapeSingleStack :: Shape -> Shape -> Axis -> Shape
-shapeSingleStack (a : as) (b : bs) axis = f (a : as) (b : bs) axis 0
-  where
-    f :: Shape -> Shape -> Axis -> Axis -> Shape
-    f [] [] target cur = if target == cur then [2] else error "not enough axes"
-    f (c : cs) (d : ds) target cur
-        | target == cur =
-            if cs == ds && c == d
-                then (2 : (c : cs))
-                else error "shape mismatch"
-        | target > cur =
-            if (c : cs) == (d : ds)
-                then c : f cs ds target (cur + 1)
-                else error "shape mismatch"
-        | otherwise = error "Not enough dimensions"
-    f _ _ _ _ = error "Shouldn't Happen"
-shapeSingleStack _ _ _ = error "Shape should be non empty list"
-
-shapeStack :: [Shape] -> Axis -> Shape
-shapeStack (shape : shapes) axis = foldl (\x y -> shapeSingleStack x y axis) shape shapes
-shapeStack [] _ = error "no shapes to stack"
+    (a : (b : _)) = reverse shape1
+    (c : (d : _)) = reverse shape2
+    (Array _ newshape) = mul (Array Af32 [a, b]) (Array Af32 [c, d])
+mul _ _ = error "Not implemented"
