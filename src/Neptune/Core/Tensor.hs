64a4b43b53163439abcf655982c8885a387d69f2
mzguntalan <mzguntalan@gmail.com>
2025-01-24 19:17:42 +0900

    Reference to myself: Playing with classes for Tensors and TensorTypes

--- src/Neptune/Core/Tensor.hs
+++ src/Neptune/Core/Tensor.hs
@@ -1,22 +1,53 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 module Neptune.Core.Tensor where
+
+import Jaxpr (BlxTensor)
+import Jaxpr.Blx.Tensor qualified as Blx
 
 class TensorType a where
     symbol :: a -> String
 
 -- a tensor is a vector
 class (TensorType b) => BehavesLikeATensor a b where
-    shape :: a b -> [Int]
-    promoteToShape :: a b -> [Int] -> a b
-    nameOf :: a b -> String
-    tensorType :: a b -> b
-    castTo :: (TensorType c) => a b -> a c
-    renameTensor :: a b -> String -> a b
+    -- shape :: a -> [Int]
+    -- promoteToShape :: a -> [Int] -> a
+    -- nameOf :: a -> String
+    tensorType :: a -> b
+    castTo :: a -> b -> a
 
-rankOf :: (BehavesLikeATensor a b) => a b -> Int
-rankOf = length . shape
+-- renameTensor :: a -> String -> a
+-- addTensor :: a -> a -> a
+-- subTensor :: a -> a -> a
+-- elementWiseMultiply :: a -> a -> a
 
-tensorCopyWithNewName :: (BehavesLikeATensor a b) => a b -> String -> a b
-tensorCopyWithNewName = renameTensor
+-- rankOf :: (BehavesLikeATensor a b) => a b -> Int
+-- rankOf = length . shape
 
-showTensor :: (BehavesLikeATensor a b) => a b -> String
-showTensor t = nameOf t ++ "[" ++ show (shape t) ++ "]"
+-- tensorCopyWithNewName :: (BehavesLikeATensor a b) => a b -> String -> a b
+-- tensorCopyWithNewName = renameTensor
+
+-- showTensor :: (BehavesLikeATensor a b) => a b -> String
+-- showTensor t = nameOf t ++ "[" ++ show (shape t) ++ "]"
+
+instance TensorType Blx.TensorType where
+    symbol = show
+
+data StringType = Str1 | Str2 deriving (Show)
+
+data NumType = Num1 | Num2 deriving (Show)
+
+instance TensorType StringType where symbol = show
+
+-- instance TensorType NumType where symbol = show
+
+data FakeTensor = FakeTensor String StringType deriving (Show)
+
+instance BehavesLikeATensor FakeTensor StringType where
+    castTo (FakeTensor a _) = FakeTensor a
+    tensorType (FakeTensor _ t) = t
+
+-- instance BehavesLikeATensor Blx.BlxTensor Blx.TensorType where
