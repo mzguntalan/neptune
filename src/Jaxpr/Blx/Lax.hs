4f0050821adb4e2e6c787143a76f75d257989427
mzguntalan <mzguntalan@gmail.com>
2025-01-24 09:25:59 +0900

    generalized laxApply (for single trace out)

--- src/Jaxpr/Blx/Lax.hs
+++ src/Jaxpr/Blx/Lax.hs
@@ -1,46 +1,49 @@
 module Jaxpr.Blx.Lax where
 
-import Jaxpr.Blx.Equation
+import Data.List (intercalate)
+import Jaxpr.Blx.Primitives
+import Jaxpr.Blx.Tensor
+import Jaxpr.Blx.Trace
 
 -- the following are lax mirrors
+
+-- attempt at generalizing
+laxApply :: TracePrimitive -> [BlxTrace] -> BlxTrace
+laxApply prim inputTraces
+    | numInputs prim == length inputTraces || numInputs prim == (-1) = Trace (newEntry : combinedTraceEntries) newTraceName
+    | otherwise = error "Wrong Number of Inputs"
+  where
+    newEntry = TraceEntry prim inputs outTensors
+    newTraceName = intercalate "" (map traceName inputTraces)
+    combinedTraceEntries = traceJoinEntries inputTraces
+    inputs = map (head . currentTraceOutputs) inputTraces
+    outTensors = map (`renameTensor` (newTraceName ++ "." ++ show (length combinedTraceEntries + 1))) (applyPrimitive prim inputs)
+
 labs :: BlxTrace -> BlxTrace
 labs tr = case currentTraceOutputs tr of
-    [a] -> Trace (newEquation : traceEquations tr) (traceName tr)
+    [a] -> Trace (newEquation : traceEntries tr) (traceName tr)
       where
-        newEquation :: BlxEquation
-        newEquation = BlxEquation Abs [a] outTensors
-        outTensors = map (`renameTensor` (traceName tr ++ "." ++ show ((length . traceEquations $ tr) + 1))) (primApply Abs [a])
+        newEquation :: TraceEntry
+        prim = TracePrimitive Abs
+        newEquation = TraceEntry prim [a] outTensors
+        outTensors = map (`renameTensor` (traceName tr ++ "." ++ show ((length . traceEntries $ tr) + 1))) (applyPrimitive prim [a])
     _ -> error "Too many inputs"
 
 ladd :: BlxTrace -> BlxTrace -> BlxTrace
-ladd trX trY = Trace (newEquation : equations) newTraceName
-  where
-    equations = traceJoinEquations [trX, trY]
-    newTraceName = traceName trX ++ traceName trY
-    newEquation = BlxEquation Add [x, y] outTensors
-    outTensors = map (`renameTensor` (newTraceName ++ "." ++ show (length equations + 1))) (primApply Add [x, y])
-    [x] = currentTraceOutputs trX
-    [y] = currentTraceOutputs trY
+ladd trX trY = laxApply (TracePrimitive Add) [trX, trY]
 
 lconcatenate :: [BlxTrace] -> Axis -> BlxTrace
-lconcatenate traces axis = Trace (newEquation : equations) newTraceName
-  where
-    newTraceName = intercalate "" (map traceName traces)
-    equations = traceJoinEquations traces
-    newEquation = BlxEquation prim inputs outTensors
-    inputs = map (head . currentTraceOutputs) traces
-    prim = Concatenate{concatenateDimension = axis}
-    outTensors = map (`renameTensor` (newTraceName ++ "." ++ show (length equations + 1))) (primApply prim inputs)
+lconcatenate traces axis = laxApply (TracePrimitive (Concatenate axis)) traces
 
--- limited to rank 0 tensors for now
-lbroadcastInDim :: BlxTrace -> Shape -> BlxTrace
-lbroadcastInDim tr targetShape = Trace (newEquation : equations) newTraceName
-  where
-    p = BroadcastInDim{broadcastInDimDimensions = [], broadcastInDimShape = targetShape}
-    onlyTensorInTraceOutput = case currentTraceOutputs tr of
-        [t] -> t
-        _ -> error ("There should only be 1 and only 1 tensor in the trace " ++ traceName tr)
-    outputs = map (`renameTensor` (newTraceName ++ "." ++ show (length equations + 1))) (primApply p [onlyTensorInTraceOutput])
-    newEquation = BlxEquation p [onlyTensorInTraceOutput] outputs
-    newTraceName = traceName tr
-    equations = traceEquations tr
+-- -- limited to rank 0 tensors for now
+-- lbroadcastInDim :: BlxTrace -> Shape -> BlxTrace
+-- lbroadcastInDim tr targetShape = Trace (newEquation : equations) newTraceName
+--   where
+--     p = BroadcastInDim{broadcastInDimDimensions = [], broadcastInDimShape = targetShape}
+--     onlyTensorInTraceOutput = case currentTraceOutputs tr of
+--         [t] -> t
+--         _ -> error ("There should only be 1 and only 1 tensor in the trace " ++ traceName tr)
+--     outputs = map (`renameTensor` (newTraceName ++ "." ++ show (length equations + 1))) (primApply p [onlyTensorInTraceOutput])
+--     newEquation = BlxEquation p [onlyTensorInTraceOutput] outputs
+--     newTraceName = traceName tr
+--     equations = traceEquations tr
