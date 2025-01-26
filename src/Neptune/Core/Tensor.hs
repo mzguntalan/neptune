f73ca62f6d745893f86c1adfaf4205d9728b363f
mzguntalan <mzguntalan@gmail.com>
2025-01-26 19:37:15 +0900

    Abstract m [c] as the core abstraction for everything(?)

--- src/Neptune/Core/Tensor.hs
+++ src/Neptune/Core/Tensor.hs
@@ -5,46 +5,153 @@
 
 module Neptune.Core.Tensor where
 
-import Jaxpr (BlxTensor)
-import Jaxpr.Blx.Tensor qualified as Blx
+import Data.List (intercalate)
 
-class BehaveLikeAType a where
-    symbol :: a -> String
-    castable :: a -> a -> Bool
-    convertible :: a -> a -> Bool
-    (==) :: a -> a -> Bool
+-- class BehaveLikeAType a where
+--     symbol :: a -> String
+--     castable :: a -> a -> Bool
+--     convertible :: a -> a -> Bool
+--     (==) :: a -> a -> Bool
 
--- a tensor is a vector
-class (BehaveLikeAType b) => Typed a b where
-    tensorType :: a -> b
-    castTo :: a -> b -> a
+-- -- a tensor is a vector
+-- class (BehaveLikeAType b) => Typed a b where
+--     tensorType :: a -> b
+--     castTo :: a -> b -> a
 
-class (Num b) => ConcreteTensor a b where -- No idea what this should be yet
-    valueOf :: a -> b
+-- class (Num b) => ConcreteTensor a b where -- No idea what this should be yet
+--     valueOf :: a -> b
 
-class Named a where
-    nameOf :: a -> String
-    renameTo :: a -> String -> a
+-- class Named a where
+--     nameOf :: a -> String
+--     renameTo :: a -> String -> a
 
-class StrictTensor a where -- maybe
-    shape :: a -> [Int]
+-- class StrictTensor a where -- maybe
+--     shape :: a -> [Int]
 
-    -- these are probably lax primitives
-    absTensor :: a -> a
-    addTensor :: a -> a -> a
-    concatenate :: [a] -> Int -> a
+--     -- these are probably lax primitives
+--     absTensor :: a -> a
+--     addTensor :: a -> a -> a
+--     concatenate :: [a] -> Int -> a
 
-class (StrictTensor a) => AutomaticTensor a where
-    promoteToShape :: a -> [Int] -> a
-    demoteToShape :: a -> [Int] -> a
+-- class (StrictTensor a) => AutomaticTensor a where
+--     promoteToShape :: a -> [Int] -> a
+--     demoteToShape :: a -> [Int] -> a
 
-rankOf :: (StrictTensor a) => a -> Int
-rankOf = length . shape
+-- rankOf :: (StrictTensor a) => a -> Int
+-- rankOf = length . shape
 
-tensorCopyWithNewName :: (StrictTensor a, Named a) => a -> String -> a
-tensorCopyWithNewName = renameTo
+-- tensorCopyWithNewName :: (StrictTensor a, Named a) => a -> String -> a
+-- tensorCopyWithNewName = renameTo
 
-showTensor :: (Named a, StrictTensor a) => a -> String
-showTensor t = nameOf t ++ "[" ++ show (shape t) ++ "]"
+-- showTensor :: (Named a, StrictTensor a) => a -> String
+-- showTensor t = nameOf t ++ "[" ++ show (shape t) ++ "]"
 
 -- instance BehavesLikeATensor Blx.BlxTensor Blx.TensorType where
+--
+--
+
+data Abstract m c = (Eq m, Eq c) => Abstract m [c]
+
+getM :: Abstract m c -> m
+getM (Abstract m _) = m
+
+getCs :: Abstract m c -> [c]
+getCs (Abstract _ cs) = cs
+
+abstractAreEqual :: Abstract m c -> Abstract m c -> Bool
+abstractAreEqual (Abstract m1 cs1) (Abstract m2 cs2) = m1 == m2 && cs1 == cs2
+
+class Operation a b where
+    applyOperation :: a -> [b] -> [b]
+    symbol :: a -> [b] -> String -- the symbol might be affected by the inputs::[b]; in all cases i know, it won't
+
+data Computation a = forall p. (Eq a, Operation p a) => Computation p [a] [a]
+
+instance Eq (Computation a) where
+    (Computation p1 ins1 outs1) == (Computation p2 ins2 outs2) =
+        symbol p1 ins1 == symbol p2 ins2
+            && ins1 == ins2
+            && outs1 == outs2
+
+computation :: (Eq a, Operation p a) => p -> [a] -> Computation a
+computation operation inputs = Computation operation inputs (applyOperation operation inputs)
+
+data LaxPrimitiveType = Tf32 | Ti32 | Tstr deriving (Eq, Show)
+
+data LaxTensorProperties = LaxTensorProperties [Int] LaxPrimitiveType deriving (Eq)
+
+instance Show LaxTensorProperties where
+    show (LaxTensorProperties s t) = show s ++ ":" ++ show t
+
+data AbstractLaxTensor = Abstact LaxTensorProperties (Computation LaxTensorProperties) deriving (Eq, Show)
+
+instance Show (Computation LaxTensorProperties) where
+    show (Computation p inputs outputs) = os ++ " = " ++ symbol p inputs ++ " " ++ is
+      where
+        os = "(" ++ intercalate "," (map show inputs) ++ ")"
+        is = unwords (map show outputs)
+
+applyOperationOnAbstract ::
+    (Eq m, Operation p m) => p -> [Abstract m (Computation m)] -> [Abstract m (Computation m)]
+applyOperationOnAbstract p abstractinputs = map wrap outputsMs
+  where
+    wrap o = Abstract o (newComputationByP : combinedComputations)
+    newComputationByP = computation p inputs
+    (Computation _ _ outputsMs) = newComputationByP
+    inputs = map getM abstractinputs
+
+    combinedComputations = concatMap getCs abstractinputs
+
+data Abs = Abs
+
+instance Operation Abs LaxTensorProperties where
+    applyOperation Abs [t] = [t]
+    applyOperation Abs _ = error "WrongNumberInputsError"
+    symbol Abs _ = "abs"
+
+data Add = Add
+
+instance Operation Add LaxTensorProperties where
+    applyOperation Add [x, _] = [x]
+    applyOperation Add _ = error "WrongNumberInputsError"
+    symbol Add _ = "add"
+
+newtype Concatenate = Concatenate Int
+
+instance Operation Concatenate LaxTensorProperties where
+    applyOperation (Concatenate _) [] = error "WrongNumberInputsError"
+    applyOperation (Concatenate d) (LaxTensorProperties s t : xs) = [LaxTensorProperties newshape t]
+      where
+        newshape = shapeConcat (map tensorType (LaxTensorProperties s t : xs)) d
+        tensorType (LaxTensorProperties s' _) = s'
+    symbol (Concatenate d) _ = "concatenate" ++ showAsParameters [("dimensions", d)]
+
+showAsParameters :: (Show v, Show k) => [(k, v)] -> String
+showAsParameters params = "[" ++ intercalate "," (map f params) ++ "]"
+  where
+    f (x, y) = show x ++ "=" ++ show y
+
+type Axis = Int
+
+type Shape = [Axis]
+
+shapeSingleConcat :: Shape -> Shape -> Axis -> Shape
+shapeSingleConcat (a : as) (b : bs) axis = f (a : as) (b : bs) axis 0
+  where
+    f :: Shape -> Shape -> Axis -> Axis -> Shape
+    f (c : cs) (d : ds) target cur
+        | target == cur =
+            if cs == ds
+                then c + d : cs
+                else error "shape mismatch"
+        | target > cur =
+            if c == d
+                then c : f cs ds target (cur + 1)
+                else error "shape mismatch"
+        | otherwise = error "Not enough axes"
+    f _ _ _ _ = error "Should not happen"
+shapeSingleConcat _ _ _ = error "Shape should be non empty list"
+
+shapeConcat :: [Shape] -> Axis -> Shape
+shapeConcat (shap : shapes) axis = foldl (\x y -> shapeSingleConcat x y axis) shap shapes
+shapeConcat [] _ = error "no shapes to concat"
