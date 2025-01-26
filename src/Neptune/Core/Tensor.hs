06b23030781d1e6934d23be4766c6d997b1bc5ec
mzguntalan <mzguntalan@gmail.com>
2025-01-26 16:35:51 +0900

    i have no idea; just checkpointing

--- src/Neptune/Core/Tensor.hs
+++ src/Neptune/Core/Tensor.hs
@@ -8,46 +8,46 @@ module Neptune.Core.Tensor where
 import Jaxpr (BlxTensor)
 import Jaxpr.Blx.Tensor qualified as Blx
 
-class TensorType a where
+class BehaveLikeAType a where
     symbol :: a -> String
+    castable :: a -> a -> Bool
+    convertible :: a -> a -> Bool
+    (==) :: a -> a -> Bool
 
 -- a tensor is a vector
-class (TensorType b) => BehavesLikeATensor a b where
-    -- shape :: a -> [Int]
-    -- promoteToShape :: a -> [Int] -> a
-    -- nameOf :: a -> String
+class (BehaveLikeAType b) => Typed a b where
     tensorType :: a -> b
     castTo :: a -> b -> a
 
--- renameTensor :: a -> String -> a
--- addTensor :: a -> a -> a
--- subTensor :: a -> a -> a
--- elementWiseMultiply :: a -> a -> a
+class (Num b) => ConcreteTensor a b where -- No idea what this should be yet
+    valueOf :: a -> b
 
--- rankOf :: (BehavesLikeATensor a b) => a b -> Int
--- rankOf = length . shape
+class Named a where
+    nameOf :: a -> String
+    renameTo :: a -> String -> a
 
--- tensorCopyWithNewName :: (BehavesLikeATensor a b) => a b -> String -> a b
--- tensorCopyWithNewName = renameTensor
+class StrictTensor a where -- maybe
+    shape :: a -> [Int]
 
--- showTensor :: (BehavesLikeATensor a b) => a b -> String
--- showTensor t = nameOf t ++ "[" ++ show (shape t) ++ "]"
+    -- these are probably lax primitives
+    absTensor :: a -> a
+    addTensor :: a -> a -> a
+    concatenate :: [a] -> Int -> a
+
+class (StrictTensor a) => AutomaticTensor a where
+    promoteToShape :: a -> [Int] -> a
+    demoteToShape :: a -> [Int] -> a
+
+rankOf :: (BehavesLikeATensor a b) => a b -> Int
+rankOf = length . shape
+
+tensorCopyWithNewName :: (BehavesLikeATensor a b) => a b -> String -> a b
+tensorCopyWithNewName = renameTensor
+
+showTensor :: (BehavesLikeATensor a b) => a b -> String
+showTensor t = nameOf t ++ "[" ++ show (shape t) ++ "]"
 
 instance TensorType Blx.TensorType where
     symbol = show
-
-data StringType = Str1 | Str2 deriving (Show)
-
-data NumType = Num1 | Num2 deriving (Show)
-
-instance TensorType StringType where symbol = show
-
--- instance TensorType NumType where symbol = show
-
-data FakeTensor = FakeTensor String StringType deriving (Show)
-
-instance BehavesLikeATensor FakeTensor StringType where
-    castTo (FakeTensor a _) = FakeTensor a
-    tensorType (FakeTensor _ t) = t
 
 -- instance BehavesLikeATensor Blx.BlxTensor Blx.TensorType where
