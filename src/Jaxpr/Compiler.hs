fd57b868f3c56e4255147dd08ddf71aa531b73fb
mzguntalan <mzguntalan@gmail.com>
2025-02-01 19:28:04 +0900

    Replaced everything using abstraction from Program.hs in Neptune Core

--- src/Jaxpr/Compiler.hs
+++ src/Jaxpr/Compiler.hs
@@ -1,98 +1,98 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
+-- {-# LANGUAGE DataKinds #-}
+-- {-# LANGUAGE FlexibleInstances #-}
+-- {-# LANGUAGE GADTs #-}
+-- {-# LANGUAGE MultiParamTypeClasses #-}
 
 module Jaxpr.Compiler where
 
-import Data.List (findIndex, intercalate, nub, nubBy)
-import Data.Map.Strict qualified as Map
-import Jaxpr.Blx.Primitives
-import Jaxpr.Blx.Tensor
-import Jaxpr.Blx.Trace
+-- import Data.List (findIndex, intercalate, nub, nubBy)
+-- import Data.Map.Strict qualified as Map
+-- import Jaxpr.Blx.Primitives
+-- import Jaxpr.Blx.Tensor
+-- import Jaxpr.Blx.Trace
 
-data JaxExpression = JaxExpression [BlxTensor] [BlxTensor] [TraceEntry] [BlxTensor]
+-- data JaxExpression = JaxExpression [BlxTensor] [BlxTensor] [TraceEntry] [BlxTensor]
 
-mkJaxExpression :: [BlxTensor] -> [BlxTensor] -> [TraceEntry] -> [BlxTensor] -> JaxExpression
-mkJaxExpression litInputs varInputs traceentries outputs = JaxExpression litInputs varInputs traceentries outputs
+-- mkJaxExpression :: [BlxTensor] -> [BlxTensor] -> [TraceEntry] -> [BlxTensor] -> JaxExpression
+-- mkJaxExpression litInputs varInputs traceentries outputs = JaxExpression litInputs varInputs traceentries outputs
 
-instance Show JaxExpression where
-    show (JaxExpression consts inVars entries outs) = "{ lambda " ++ constShow ++ " ; " ++ inVarsShow ++ ". let\n\t" ++ entriesShow ++ "\n in " ++ outVarsShow ++ " }"
-      where
-        constShow = unwords (map show consts)
-        inVarsShow = unwords (map show inVars)
-        entriesShow = intercalate "\n\t" (map show entries)
-        outVarsShow = "(" ++ intercalate "," (map tensorName outs) ++ ",)"
+-- instance Show JaxExpression where
+--     show (JaxExpression consts inVars entries outs) = "{ lambda " ++ constShow ++ " ; " ++ inVarsShow ++ ". let\n\t" ++ entriesShow ++ "\n in " ++ outVarsShow ++ " }"
+--       where
+--         constShow = unwords (map show consts)
+--         inVarsShow = unwords (map show inVars)
+--         entriesShow = intercalate "\n\t" (map show entries)
+--         outVarsShow = "(" ++ intercalate "," (map tensorName outs) ++ ",)"
 
-compileToJaxpr :: BlxTrace -> JaxExpression
-compileToJaxpr = prettifyJaxpr . traceToJaxExpression
+-- compileToJaxpr :: BlxTrace -> JaxExpression
+-- compileToJaxpr = prettifyJaxpr . traceToJaxExpression
 
-prettifyJaxpr :: JaxExpression -> JaxExpression
-prettifyJaxpr (JaxExpression consts inVars entries outs) = JaxExpression renamedConsts renamedInVars renamedEntries renamedOuts
-  where
-    allVars :: [BlxTensor]
-    allVars = consts ++ inVars ++ concatMap entriesAllVars entries ++ outs
-    allVarNames = nub $ map tensorName allVars
-    varnamePool = map varNameFromInt [0, 1 .. (length allVars - 1)]
-    lookupOfVarNames :: Map.Map String String
-    lookupOfVarNames = Map.fromList (zip allVarNames varnamePool)
+-- prettifyJaxpr :: JaxExpression -> JaxExpression
+-- prettifyJaxpr (JaxExpression consts inVars entries outs) = JaxExpression renamedConsts renamedInVars renamedEntries renamedOuts
+--   where
+--     allVars :: [BlxTensor]
+--     allVars = consts ++ inVars ++ concatMap entriesAllVars entries ++ outs
+--     allVarNames = nub $ map tensorName allVars
+--     varnamePool = map varNameFromInt [0, 1 .. (length allVars - 1)]
+--     lookupOfVarNames :: Map.Map String String
+--     lookupOfVarNames = Map.fromList (zip allVarNames varnamePool)
 
-    sameName :: BlxTensor -> BlxTensor -> Bool
-    sameName t1 t2 = tensorName t1 == tensorName t2
-    renamedConsts = nubBy sameName $ map (`renameTensorUsingMap` lookupOfVarNames) consts
-    renamedInVars = nubBy sameName $ map (`renameTensorUsingMap` lookupOfVarNames) inVars
-    renamedEntries = map (`renameEntriesUsingMap` lookupOfVarNames) entries
-    renamedOuts = map (`renameTensorUsingMap` lookupOfVarNames) outs
+--     sameName :: BlxTensor -> BlxTensor -> Bool
+--     sameName t1 t2 = tensorName t1 == tensorName t2
+--     renamedConsts = nubBy sameName $ map (`renameTensorUsingMap` lookupOfVarNames) consts
+--     renamedInVars = nubBy sameName $ map (`renameTensorUsingMap` lookupOfVarNames) inVars
+--     renamedEntries = map (`renameEntriesUsingMap` lookupOfVarNames) entries
+--     renamedOuts = map (`renameTensorUsingMap` lookupOfVarNames) outs
 
-traceToJaxExpression :: BlxTrace -> JaxExpression
-traceToJaxExpression tr = JaxExpression consts inVars entries outVars
-  where
-    allTraceEntries = reverse . traceEntries $ tr
-    entries = filter isJaxExpressionEntry allTraceEntries
-    isJaxExpressionEntry :: TraceEntry -> Bool
-    isJaxExpressionEntry x = not (isLitEntry x) && not (isVarEntry x)
+-- traceToJaxExpression :: BlxTrace -> JaxExpression
+-- traceToJaxExpression tr = JaxExpression consts inVars entries outVars
+--   where
+--     allTraceEntries = reverse . traceEntries $ tr
+--     entries = filter isJaxExpressionEntry allTraceEntries
+--     isJaxExpressionEntry :: TraceEntry -> Bool
+--     isJaxExpressionEntry x = not (isLitEntry x) && not (isVarEntry x)
 
-    litEntries = filter isLitEntry allTraceEntries
-    varEntries = filter isVarEntry allTraceEntries
+--     litEntries = filter isLitEntry allTraceEntries
+--     varEntries = filter isVarEntry allTraceEntries
 
-    consts = map (head . entryOutputs) litEntries
-    inVars = map (head . entryInputs) varEntries
+--     consts = map (head . entryOutputs) litEntries
+--     inVars = map (head . entryInputs) varEntries
 
-    outVars = currentTraceOutputs tr
+--     outVars = currentTraceOutputs tr
 
-renameTensorUsingMap :: BlxTensor -> Map.Map String String -> BlxTensor
-renameTensorUsingMap t m = case Map.lookup (tensorName t) m of
-    Just newName -> renameTensor t newName
-    Nothing -> error "Name not found in lookup"
+-- renameTensorUsingMap :: BlxTensor -> Map.Map String String -> BlxTensor
+-- renameTensorUsingMap t m = case Map.lookup (tensorName t) m of
+--     Just newName -> renameTensor t newName
+--     Nothing -> error "Name not found in lookup"
 
-renameEntriesUsingMap :: TraceEntry -> Map.Map String String -> TraceEntry
-renameEntriesUsingMap (TraceEntry prim inputs outputs) varmap = TraceEntry prim renamedInputs renamedOutputs
-  where
-    f :: BlxTensor -> BlxTensor
-    f t = renameTensor t newName
-      where
-        newName = case Map.lookup (tensorName t) varmap of
-            Just a -> a
-            Nothing -> error "You made a mistake with generating the correspondences of oldnames to new names"
+-- renameEntriesUsingMap :: TraceEntry -> Map.Map String String -> TraceEntry
+-- renameEntriesUsingMap (TraceEntry prim inputs outputs) varmap = TraceEntry prim renamedInputs renamedOutputs
+--   where
+--     f :: BlxTensor -> BlxTensor
+--     f t = renameTensor t newName
+--       where
+--         newName = case Map.lookup (tensorName t) varmap of
+--             Just a -> a
+--             Nothing -> error "You made a mistake with generating the correspondences of oldnames to new names"
 
-    renamedInputs = map f inputs
-    renamedOutputs = map f outputs
+--     renamedInputs = map f inputs
+--     renamedOutputs = map f outputs
 
-entriesAllVars :: TraceEntry -> [BlxTensor]
-entriesAllVars entry = entryInputs entry ++ entryOutputs entry
+-- entriesAllVars :: TraceEntry -> [BlxTensor]
+-- entriesAllVars entry = entryInputs entry ++ entryOutputs entry
 
-symbolsForNaming :: String
-symbolsForNaming = "abcdefghijklmnopqrstuvwxyz"
+-- symbolsForNaming :: String
+-- symbolsForNaming = "abcdefghijklmnopqrstuvwxyz"
 
--- this needs fixing
-varNameFromInt :: Int -> String
-varNameFromInt x
-    | 0 <= x && x < length symbolsForNaming = [symbolsForNaming !! x]
-    | x >= length symbolsForNaming = "a" ++ varNameFromInt (x - length symbolsForNaming)
-    | otherwise = error "Shouldn't happen"
+-- -- this needs fixing
+-- varNameFromInt :: Int -> String
+-- varNameFromInt x
+--     | 0 <= x && x < length symbolsForNaming = [symbolsForNaming !! x]
+--     | x >= length symbolsForNaming = "a" ++ varNameFromInt (x - length symbolsForNaming)
+--     | otherwise = error "Shouldn't happen"
 
-isVarEntry :: TraceEntry -> Bool
-isVarEntry (TraceEntry prim _ _) = prim == TracePrimitive Var
+-- isVarEntry :: TraceEntry -> Bool
+-- isVarEntry (TraceEntry prim _ _) = prim == TracePrimitive Var
 
-isLitEntry :: TraceEntry -> Bool
-isLitEntry (TraceEntry prim _ _) = prim == TracePrimitive Lit
+-- isLitEntry :: TraceEntry -> Bool
+-- isLitEntry (TraceEntry prim _ _) = prim == TracePrimitive Lit
---
+++ src/Jaxpr/Lax.hs
@@ -0,0 +1,63 @@
+module Jaxpr.Lax where
+
+import Neptune.Core.Program
+
+-- Abstract Tensors
+
+data TensorOrigin a = (Show a, Eq a) => FromValue a | FromAbstract | FromComputation
+
+instance Show (TensorOrigin a) where
+    show FromAbstract = "FromAbstract"
+    show (FromValue a) = "Val:" ++ show a
+    show FromComputation = "FromComputation"
+
+instance Eq (TensorOrigin a) where
+    FromAbstract == FromAbstract = False
+    FromValue a1 == FromValue a2 = a1 == a2
+    FromComputation == FromComputation = False
+    _ == _ = False
+
+type Shape = [Int]
+
+data TensorType = Tf32 | Ti32 deriving (Eq, Show)
+
+data TensorDescription = TensorDescription Shape TensorType (TensorOrigin String) deriving (Eq, Show)
+
+data LaxPrimitive = LaxPrimitive String [(String, String)] TensorDescription deriving (Eq, Show)
+
+type Tensor = Program LaxPrimitive
+
+laxprimitive :: String -> [(String, String)] -> TensorDescription -> PrimitiveInstruction LaxPrimitive
+laxprimitive primName primParams primOutputDescription = PrimitiveInstruction (LaxPrimitive primName primParams primOutputDescription)
+
+abstractTensor :: Shape -> TensorType -> Tensor
+abstractTensor shape ty = Immediate (laxprimitive "abtract_tensor" [] (TensorDescription shape ty FromAbstract)) []
+
+getShape :: Tensor -> Shape
+getShape t = s
+  where
+    PrimitiveInstruction (LaxPrimitive _ _ (TensorDescription s _ _)) = lastPrimitiveInstructionInProgram t
+
+getType :: Tensor -> TensorType
+getType t = tensorType
+  where
+    PrimitiveInstruction (LaxPrimitive _ _ (TensorDescription _ tensorType _)) = lastPrimitiveInstructionInProgram t
+
+ladd :: Tensor -> Tensor -> Tensor
+ladd a b
+    | getShape a == getShape b = Immediate (PrimitiveInstruction (LaxPrimitive "add" [] (TensorDescription shape ty FromComputation))) [a, b]
+    | otherwise = error "Shape"
+  where
+    shape = getShape a
+    ty = getType a
+
+isNumeric :: Tensor -> Bool
+isNumeric a = getType a `elem` [Tf32, Ti32]
+
+labs :: Tensor -> Tensor
+labs a
+    | isNumeric a = Immediate (PrimitiveInstruction (LaxPrimitive "abs" [] (TensorDescription shape ty FromComputation))) [a]
+    | otherwise = error "not a number"
+  where
+    shape = getShape a
+    ty = getType a
